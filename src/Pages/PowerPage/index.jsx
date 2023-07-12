33cc3d954d784426837bda263e4266091ea109ff
Viktor Pavlovic <viktorpavlovic117@gmail.com>
2023-07-12 22:15:50 +0200

    final ranking logic

--- src/Pages/PowerPage/index.jsx
+++ src/Pages/PowerPage/index.jsx
@@ -1,14 +1,15 @@
 import { React, useEffect, useState } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, getDoc,doc } from "firebase/firestore";
 import { db } from "../../firebase";
 import Header from "../../Components/Header";
 import Footer from "../../Components/Footer";
 import SelectingTeams from "../../Components/SelectingTeams";
 import "./power-page.scss";
 
-const PowerPage = ({uid}) => {
+const PowerPage = ({ uid }) => {
   const [allTeams, setAllTeams] = useState([]);
-  const [allUsers,setAllUsers] = useState([])
+  const [allUsersTeams, setAllUsersTeams] = useState([]);
+  console.log(allUsersTeams)
 
   useEffect(() => {
     const fetchAllTeams = async () => {
@@ -22,6 +23,7 @@ const PowerPage = ({uid}) => {
     };
     fetchAllTeams();
   }, []);
+  
   useEffect(() => {
     const fetchAllUsers = async () => {
       const collectionRef = collection(db, "users");
@@ -30,11 +32,53 @@ const PowerPage = ({uid}) => {
         id: doc.id,
         data: doc.data(),
       }));
-      setAllUsers(usersData);
+
+      const userTeams = {};
+
+      for (const user of usersData) {
+        const userName = user.data.userName;
+        const userRef = doc(db, "users", user.id);
+        const userDoc = await getDoc(userRef);
+        const userData = userDoc.data();
+        const userTeamsArray = userData.teams || []; 
+        userTeams[userName] = userTeamsArray;
+      }
+
+      setAllUsersTeams(userTeams);
     };
+
     fetchAllUsers();
   }, []);
+  const calculateTeamPoints = () => {
+    const teamPoints = {};
   
+    for (const user in allUsersTeams) {
+      const teams = allUsersTeams[user];
+  
+      teams.forEach((team, index) => {
+        const points = teams.length - index;
+        
+        if (teamPoints.hasOwnProperty(team)) {
+          teamPoints[team] += points;
+        } else {
+          teamPoints[team] = points;
+        }
+      });
+    }
+  
+    return teamPoints;
+  };
+  
+  const teamPoints = calculateTeamPoints();
+  const sortedTeamPoints = Object.entries(teamPoints)
+  .sort((a, b) => b[1] - a[1])
+  .reduce((obj, [key, value]) => {
+    obj[key] = value;
+    return obj;
+  }, {});
+
+console.log(sortedTeamPoints);
+
   return (
     <div className="div-power-page">
       <Header />
